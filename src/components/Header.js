3aeab5cd5d6ef09f03d48ec4753e9a55c6dea0fa
maquino96 <matthewaquino96@gmail.com>
2021-03-17 16:40:12 -0400

    commit tree c4db68be98ab7d0398fc633ff2ed7d1ba3d82a76

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+
+function Header ({onDarkModeClick, mode}) {
+    return(
+        <header>
+            <h2>Shopster</h2>
+            <button onClick={onDarkModeClick}>
+            {mode ? "Dark" : "Light"} Mode
+            </button>
+        </header>
+    )
+}
+
+
+export default Header
